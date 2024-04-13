fbd9e0550277a3113b82b39f2a9aa8d416f33d56
Anto Akoke Victor <antoakoke@gmail.com>
2024-04-13 10:19:48 +0100

    modify users and create profiles migration

---
+++ database/migrations/2024_04_13_091242_create_profiles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->foreignIdFor(User::class)->onDelete('cascade');
+            $table->string('dob')->nullable(false);
+            $table->string('state')->nullable(false);
+            $table->string('lga')->nullable(false);
+            $table->string('gender')->nullable(false);
+            $table->string('type')->nullable(false);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('profiles');
+    }
+};
