eae3210e5ad92c06405c560bda8a59de1d5623ea
Anto Akoke Victor <antoakoke@gmail.com>
2024-04-14 14:51:10 +0100

    create an option for a question

--- app/Http/Controllers/API/OptionController.php
+++ app/Http/Controllers/API/OptionController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Base\BaseController;
+use App\Http\Requests\StoreOptionRequest;
 use App\Http\Resources\{OptionResource};
 use App\Models\{Question};
 use App\Services\Assessment\QuestionService;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -15,11 +17,18 @@ class OptionController extends BaseController
         public QuestionService $questionService
     ){}
 
-    public function index(Question $question)
+    public function index(Question $question): JsonResponse
     {
         if($question->options === null) {
             return $this->ok(null, 'Options successfully retrieved', Response::HTTP_OK);
         }
         return $this->ok(OptionResource::collection($question->options), 'Options successfully retrieved', Response::HTTP_OK);
+    }
+
+    public function store(StoreOptionRequest $request, Question $question): JsonResponse
+    {
+        $request = $request->only(['content', 'isAnswer']);
+        $option = $this->questionService->create_option($request, $question);
+        return $this->ok(new OptionResource($option), 'Options successfully created', Response::HTTP_CREATED);
     }
 }
